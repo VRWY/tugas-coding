88b139d871826ed2b163a36ae384ab1eb4e7cd5b
VRWY <113221647+VRWY@users.noreply.github.com>
2022-09-14 12:00:30 +0700

    Add files via upload

---
+++ tugascoding.py
@@ -0,0 +1,26 @@
+print("MENGHITUNG KUBUS DAN BALOK")
+#luas kubus
+print("Menghitung luas kubus")
+s = ("panjang rusuk = 4")
+rumus = ("Rumus luas kubus adalah 6 * s * s")
+LK = 6*4*4
+luas = ("Luas kubus adalah", LK)
+
+#luas balok
+balok = ("Menghitung luas balok")
+p = ("panjang = 8")
+l = ("laus = 6")
+t = ("tinggi = 7")
+luas_balok = ("Rumus luas balok adalah 2 * (pl + lt + pt)")
+LB = 2*(8*6 + 6*7 + 8*7)
+Luas = ("Luas balok adalah", LB)
+
+print(s)
+print(rumus)
+print(luas)
+print(balok)
+print(p)
+print(l)
+print(t)
+print(luas_balok)
+print(Luas)
